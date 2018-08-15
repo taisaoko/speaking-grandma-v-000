fbdd1afe5e6da85fc7bb1d7c5536e844fa8575cc
taisaoko <ttd3calvin@gmail.com>
2018-08-15 15:03:45 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -4,10 +4,10 @@ speak_to_grandma(phrase)
     puts "HUH?! SPEAK UP, SONNY!"
   elsif 
     puts "NO, NOT SINCE 1938!."
-  elsif
+  elsif phrase == 
     puts "I LOVE YOU TOO PUMPKIN!"
   end
-
+end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
