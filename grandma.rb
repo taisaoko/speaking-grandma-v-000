a387b4f41e4f2d36ba31247e254c27ff0242528b
taisaoko <ttd3calvin@gmail.com>
2018-08-15 17:53:51 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
-  if phrase.upcase?
+  if phrase == phrase.upcase
     puts "HUH?! SPEAK UP, SONNY!"
   elsif phrase == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
